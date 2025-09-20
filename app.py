2df422b083c96dc483d44f0f86497a51fa6e91df
skeatw555 <skz.k@mail.ru>
2025-09-20 21:19:29 +0300

    parse_class

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
-from parser import return_news
+from parser_class import ParseData
 
 app = Flask(__name__)
+parse_date = ParseData()
 
 
 @app.route('/')
 def print_news():
-	news = return_news()
+	news = parse_date.return_news_from_file()
+	print(news[0])
 	return render_template('index.html', news=news)
 
 
---
+++ parser_class.py
@@ -0,0 +1,72 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import random
+from urllib.parse import urljoin
+import csv
+from datetime import datetime
+import os
+
+class ParseData:
+
+    def __init__(self) -> None:
+        self.filename = 'data.csv'
+
+    def get_list_news(self):
+        time.sleep(random.uniform(0.5, 1.5))
+
+        response = requests.get('https://lenta.ru').text
+        soup = BeautifulSoup(response, 'html.parser')
+        div_topnews_columns = soup.find_all('div', class_='topnews__column')
+
+        news = []
+
+        for column in div_topnews_columns:
+            all_a = column.find_all('a')    # type: ignore
+
+            for a in all_a:
+                href = a.get('href')    # type: ignore
+                if href:
+                    href = urljoin('https://lenta.ru', href)   # type: ignore
+
+                time_tag = a.find('time')   # type: ignore
+                time_news = time_tag.text if time_tag else 'None'   # type: ignore
+
+                for t in a.find_all('time'):    # type: ignore
+                    t.decompose()
+
+                description = a.text.strip()
+
+                news.append([description, time_news, datetime.now().date() ,href])
+        
+        return news[:-1]
+    
+    def write_to_file(self, news):
+        links = []
+        empty_file = True if os.path.getsize(self.filename) == 0 else False
+
+        if not empty_file:
+            with open(self.filename, 'r', encoding='utf-8') as file_read:
+                data_for_file = list(csv.reader(file_read))
+                for item in data_for_file:
+                    links.append(item[3])
+        
+        with open(self.filename, 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            for row in news:
+                if row[3] not in links:
+                    writer.writerow(row)
+
+    def return_news_from_file(self):
+        news = self.get_list_news()
+        self.write_to_file(news)
+        result = []
+        with open(self.filename, 'r', encoding='utf-8') as file_read:
+            rider = list(csv.reader(file_read))
+            for row in rider:
+                result.append([row[0], row[1], row[3]])
+        
+        return result
+    
+p = ParseData()
+print(p.return_news_from_file()[0])
