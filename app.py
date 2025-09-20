78da1c03e0ee2013a33f90628a2e9847ec4a9d2b
skeatw555 <skz.k@mail.ru>
2025-09-20 15:50:10 +0300

    ERRORS

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 from parser import return_news
 
 app = Flask(__name__)
-news = return_news()
 
 
 @app.route('/')
 def print_news():
+	news = return_news()
 	return render_template('index.html', news=news)
 
 
+if __name__ == '__main__':
+	app.run(debug=True)
--- parser.py
+++ parser.py
@@ -2,30 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import random
-
-
-time.sleep(random.uniform(0.5, 1.5))
-response = requests.get('https://lenta.ru').text
-soup = BeautifulSoup(response, 'html.parser')
-
-div_topnews_column = soup.find_all('div', class_='topnews__column')
+from urllib.parse import urljoin
 
 
 def return_news():
+	time.sleep(random.uniform(0.5, 1.5))
+	response = requests.get('https://lenta.ru').text
+	soup = BeautifulSoup(response, 'html.parser')
+
+	div_topnews_column = soup.find_all('div', class_='topnews__column')
+
 	news = []   # ["описание", "время", "ссылка"]
 
 	for column in div_topnews_column:
 		all_a = column.find_all('a')
 		for a in all_a:
-			href = a['href']
-			time_news = a.find('time').text
+			href = a.get('href')
+			if href:
+				href = urljoin('https://lenta.ru', href)
+
+			time_tag = a.find('time')
+			time_news = time_tag.text if time_tag else 'None'
+
 			for t in a.find_all('time'):
 				t.decompose()
-			description = a.text
+
+			description = a.text.strip()
 
 			news.append([description, time_news, href])
 
-	return news[:-1]
-
-
-print(return_news())
+	return news
